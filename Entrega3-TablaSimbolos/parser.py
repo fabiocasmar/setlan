92696d9c5932e669e25e9d9423d56e4a0e2bcd30
tony <tonyaleja@gmail.com>
2015-02-18 18:53:12 -0430

    modificado el parser hasta operadores

--- Entrega3-TablaSimbolos/parser.py
+++ Entrega3-TablaSimbolos/parser.py
@@ -11,11 +11,33 @@ from lexer import tokens, lexer_error, find_column
 from ast import *
 
 
+# To get position span for a specified symbol,
+# line and column for start and end
+def span(symbol, pos):
+    if isinstance(symbol[pos], (int, str)):
+        lexspan = symbol.lexspan(pos)
+        linespan = symbol.linespan(pos)
+
+        startpos = linespan[0], find_column(lexer.lexdata, lexspan[0])
+        endpos = linespan[1], find_column(symbol.lexer.lexdata, lexspan[1])
+    elif isinstance(symbol[pos], list):
+        startpos, _ = symbol[pos][0].lexspan
+        _, endpos = symbol[pos][-1].lexspan
+    else:
+        startpos, endpos = symbol[pos].lexspan
+    return startpos, endpos
+
+
+
+
+
 # Primera regla a evaluar
 # Un programa Setlan siempre comienza con la palabra reservada 'program'
 def p_program(symbol):
-    "program : PROGRAM statement"
-    symbol[0] = Program(symbol[2])
+    """program : PROGRAM statement"""
+    start, _ = span(symbol, 1)
+    _, end = span(symbol, 2)
+    symbol[0] = Program((start, end), symbol[2])
 
 ###############################################################################
 #############################     STATEMENTS      #############################
@@ -25,8 +47,11 @@ def p_program(symbol):
 # Declaracion de asignacion
 # ID '=' expresion
 def p_statement_assing(symbol):
-    "statement : ID ASSIGN expression"
-    symbol[0] = Assign(Variable(symbol[1]), symbol[3])
+    """statement : ID ASSIGN expression"""
+    variable = Variable(span(symbol, 1), symbol[1])
+    start, _ = span(symbol, 1)
+    _, end = span(symbol, 3)
+    symbol[0] = Assign((start, end), variable, symbol[3])
 
 
 
@@ -37,20 +62,52 @@ def p_statement_block(symbol):
     """statement : OPENCURLY statement_list CLOSECURLY
                  | OPENCURLY USING declare_list IN statement_list CLOSECURLY"""
     if len(symbol) == 4:
-        symbol[0] = Block(symbol[2])
+        start, _ = span(symbol, 1)
+        _, end = span(symbol, 3)
+        symbol[0] = Block((start, end), symbol[2])
     else:
-        # symbol[0] = Block(symbol[4], symbol[3])
-        symbol[0] = Block(symbol[5])
+        # # symbol[0] = Block(symbol[4], symbol[3])
+        # symbol[0] = Block(symbol[5])
+        start, _ = span(symbol, 1)
+        _, end = span(symbol, 6)
+        symbol[0] = Block((start, end), symbol[5], symbol[3])
 
 
 # Regla de la gramatica para declarar el tipo de una variable
 def p_statement_declare_list(symbol):
     """declare_list : data_type declare_comma_list SEMICOLON
                     | declare_list data_type declare_comma_list SEMICOLON"""
+    
+    def error_already_declared(variable, scope, data_type):
+        message = "ERROR: declaring variable '%s' of type '%s' at "
+        message += "line %d, column %d with previous declaration "
+        message += "of type '%s' at line %d, column %d"
+        old_value = scope.find(variable)
+        old_lin, old_col = old_value.lexspan[0]
+        new_lin, new_col = variable.lexspan[0]
+        data = (variable.name, data_type, new_lin, new_col,
+                old_value.data_type, old_lin, old_col)
+        static_error.append(message % data)
+
+
     if len(symbol) == 4:
-        symbol[0] = [(symbol[2], symbol[1])]
+        scope = SymTable()
+        for var in symbol[2]:
+            if scope.is_local(var):
+                error_already_declared(var, scope, symbol[1])
+            else:
+                scope.insert(var, symbol[1])
+        symbol[0] = scope
+        # symbol[0] = [(symbol[2], symbol[1])]
     else:
-        symbol[0] = symbol[1] + [(symbol[3], symbol[2])]
+        scope = symbol[1]
+        for var in symbol[3]:
+            if scope.is_local(var):
+                error_already_declared(var, scope, symbol[2])
+            else:
+                scope.insert(var, symbol[2])
+        symbol[0] = scope
+        # symbol[0] = symbol[1] + [(symbol[3], symbol[2])]
 
 
 # Regla para crear una lista con el nombre de las variables
@@ -58,9 +115,9 @@ def p_statement_declare_comma_list(symbol):
     """declare_comma_list : ID
                           | declare_comma_list COMMA ID"""
     if len(symbol) == 2:
-        symbol[0] = [Variable(symbol[1])]
+        symbol[0] = [Variable(span(symbol, 1), symbol[1])]
     else:
-        symbol[0] = symbol[1] + [Variable(symbol[3])]
+        symbol[0] = symbol[1] + [Variable(span(symbol, 3), symbol[3])]
 
 
 # Las declaraciones se separan por un ';'
@@ -78,7 +135,7 @@ def p_data_type(symbol):
     """data_type : INT
                  | BOOL
                  | SET """
-    symbol[0] = symbol[1]
+    symbol[0] = symbol[1].upper()
 
 ###############################     IN/OUT      ###############################
 
@@ -86,20 +143,23 @@ def p_data_type(symbol):
 # Declaracion scan, se aplica sobre una variable 
 def p_statement_scan(symbol):
     "statement : SCAN ID"
-    symbol[0] = Scan(Variable(symbol[2]))
+    variable = Variable(span(symbol, 2), symbol[2])
+    start, _ = span(symbol, 1)
+    _, end = span(symbol, 2)
+    symbol[0] = Read((start, end), variable)
 
 
 # Comando 'print', muestra por pantalla las expresiones dadas
 def p_statement_print(symbol):
     """statement : PRINT comma_list
                  | PRINTLN comma_list"""
+    start, _ = span(symbol, 1)
+    _, end = span(symbol, 2)
     if symbol[1].upper() == 'PRINT':
-        symbol[0] = Print(symbol[2])
+        symbol[0] = Print((start, end), symbol[2])
     else:
-        if len(symbol) == 3:
-            symbol[0] = Print(symbol[2] + [String('"\\n"')])
-        else:
-            symbol[0] = Print([String('"\\n"')])
+        symbol[0] = PrintLn((start, end), symbol[2])
+    # revisar si cambio algo en su WRITE
 
 
 # Lista de elementos a imprimir con la funcion 'print' 
@@ -111,6 +171,18 @@ def p_statement_comma_list(symbol):
     else:
         symbol[0] = symbol[1] + [symbol[3]]
 
+# # A string is a valid printable
+# def p_print_string_literal(symbol):
+#     """printable : STRING"""
+#     symbol[0] = String(span(symbol, 1), symbol[1])
+# REVISAR
+
+# # An expression is a valid printable
+# def p_exp_print(symbol):
+#     """printable : expression"""
+#     symbol[0] = symbol[1]
+# REVISAR
+
 ############################     CONDICIONALES      #############################
 
 
@@ -119,9 +191,15 @@ def p_statement_if(symbol):
     """statement :  IF OPENPAREN expression CLOSEPAREN  statement ELSE statement
                   |   IF OPENPAREN expression CLOSEPAREN  statement  """
     if len(symbol) == 6:
-        symbol[0] = If(symbol[3], symbol[5])
+        start, _ = span(symbol, 1)
+        _, end = span(symbol, 5)
+        symbol[0] = If((start, end), symbol[3], symbol[5])
+        # symbol[0] = If(symbol[3], symbol[5])
     else:
-        symbol[0] = If(symbol[3], symbol[5], symbol[7])
+        start, _ = span(symbol, 1)
+        _, end = span(symbol, 7)
+        symbol[0] = If((start, end), symbol[3], symbol[5], symbol[7])
+        # symbol[0] = If(symbol[3], symbol[5], symbol[7])
 
 
 ###############################     LAZOS      #################################
@@ -131,26 +209,37 @@ def p_statement_if(symbol):
 def p_statement_for(symbol):
     """statement : FOR ID MAX expression DO statement
                  | FOR ID MIN expression DO statement"""
-    symbol[0] = For(Variable(symbol[2]), symbol[4], symbol[6], symbol[3])
-
+    # symbol[0] = For(Variable(symbol[2]), symbol[4], symbol[6], symbol[3])
+    variable = Variable(span(symbol, 2), symbol[2])
+    start, _ = span(symbol, 1)
+    _, end = span(symbol, 6)
+    symbol[0] = For((start, end), variable, symbol[4], symbol[6])
 
 # Declaracion while-do, despues de pasar el chequeo de guarda,
 # se realizan las expresiones en el bloque
 def p_statement_while(symbol):
     "statement : WHILE OPENPAREN expression CLOSEPAREN DO statement"
-    symbol[0] = While(symbol[3], symbol[6])
+    start, _ = span(symbol, 1)
+    _, end = span(symbol, 6)
+    symbol[0] = While((start, end), symbol[3], symbol[6])
 
 # Declaracion repeat-while, realiza las instruciones en el bloque,
 # luego evalua la expresion en el while, si se cumple vuelve al bloque
 def p_statement_repeat(symbol):
     "statement : REPEAT statement WHILE OPENPAREN expression CLOSEPAREN "
-    symbol[0] = Repeat(symbol[2], symbol[5])
+    # symbol[0] = Repeat(symbol[2], symbol[5])
+    start, _ = span(symbol, 1)
+    _, end = span(symbol, 6)
+    symbol[0] = Repeat((start, end), symbol[2], symbol[5])
 
 
 # Combinacion de ambos lazos
 def p_statement_repeat_while(symbol):
     "statement : REPEAT statement WHILE OPENPAREN expression CLOSEPAREN DO statement"
-    symbol[0] = RepeatWhile(symbol[2], symbol[5], symbol[8])
+    start, _ = span(symbol, 1)
+    _, end = span(symbol, 8)
+    symbol[0] = RepeatWhile((start, end), symbol[2], symbol[5], symbol[8])
+    # symbol[0] = RepeatWhile(symbol[2], symbol[5], symbol[8])
 
 
 ###############################################################################
@@ -191,20 +280,22 @@ precedence = (
 # Numeros
 def p_exp_int_literal(symbol):
     "expression : NUMBER"
-    symbol[0] = Int(symbol[1])
-
+    symbol[0] = Int(span(symbol, 1), symbol[1])
 
 # Booleanos
 def p_exp_bool_literal(symbol):
     """expression : TRUE
                   | FALSE"""
-    symbol[0] = Bool(symbol[1].upper())
+    symbol[0] = Bool(span(symbol, 1), symbol[1].upper())
 
 
 # Conjuntos
 def p_exp_set_literal(symbol):
     "expression : OPENCURLY comma_list CLOSECURLY"
-    symbol[0] = Set(symbol[2])
+    # symbol[0] = Set(symbol[2])
+    start, _ = span(symbol, 1)
+    _, end = span(symbol, 3)
+    symbol[0] = Set((start, end), symbol[2])
 
 
 # Cadena de caracteres
@@ -216,13 +307,16 @@ def p_exp_string_literal(symbol):
 # Variables
 def p_expression_id(symbol):
     "expression : ID"
-    symbol[0] = Variable(symbol[1])
+    symbol[0] = Variable(span(symbol, 1), symbol[1])
 
 
 # Expresiones entre parentesis
 def p_expression_group(symbol):
     """expression : OPENPAREN expression CLOSEPAREN"""
     symbol[0] = symbol[2]
+    start, _ = span(symbol, 1)
+    _, end = span(symbol, 3)
+    symbol[0].lexspan = start, end
 
 #############################    OPERADORES     ###############################
 
